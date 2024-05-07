4554168d86397f13f04ac7d33b3eb696c857ea23
FranciscoQuezadaR <87053851+FranciscoQuezadaR@users.noreply.github.com>
2024-05-06 23:48:24 -0600

    commit

--- AIModelGPS/app.py
+++ AIModelGPS/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, request, redirect, Response, url_for, session
 import os
 from flask_mysqldb import MySQL,MySQLdb # pip install Flask-MySQLdb
+from flask import current_app
 
 app = Flask(__name__,template_folder='template')
 
@@ -14,17 +15,14 @@ mysql = MySQL(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html')   
-
-@app.route('/admin')
-def admin():
-    return render_template('admin.html')   
+    if 'logueado' in session and session['logueado'] == True:
+        return render_template('admin.html')
+    else:
+        return render_template('index.html')  
 
 @app.route('/acceso-login', methods= ["GET", "POST"])
 def login():
-   
     if request.method == 'POST' and 'txtUsuario' in request.form and 'txtCorreo' in request.form and 'txtPassword' in request.form:
-       
         _usuario = request.form['txtUsuario']
         _correo = request.form['txtCorreo']
         _password = request.form['txtPassword']
@@ -36,31 +34,44 @@ def login():
         if account:
             session['logueado'] = True
             session['idUsuario'] = account['idUsuario']
-
             return render_template("admin.html")
         else:
             return render_template('index.html',mensaje="Usuario o Contraseña Incorrectas")
 
+@app.route('/cerrar-sesion', methods= ["GET", "POST"])
+def signout():
+    session.pop('logueado', None)  # Remove 'logueado' key from session
+    session.pop('idUsuario', None)  # Remove 'idUsuario' key from session
+    return redirect(url_for('home'))  # Redirect to home page after logout
+
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    file = request.files['file']
-    if file:
-        filename = file.filename
+    if 'logueado' in session and session['logueado'] == True:
 
-        # Carpeta uploads en la app flask (host)
-        file.save(os.path.join('uploads', filename))
+        file = request.files['file']
+        if file:
+            filename = file.filename
+            uploads_dir = os.path.join(current_app.root_path, 'uploads')
+            if not os.path.exists(uploads_dir):
+                os.makedirs(uploads_dir)
 
-        # Guardar la url en la base de datos
-        cur = mysql.connection.cursor()
-        cur.execute('INSERT INTO imagen (nombre, formato, ruta, usuarioidUsuario) VALUES (%s, %s, %s, %s)',
-                   (filename, 'jpg', f"/uploads/{filename}", 1))
-        mysql.connection.commit()
-        
-    return render_template('admin.html')
+            file_path = os.path.join(uploads_dir, filename)
+            file.save(file_path)
+
+            cur = mysql.connection.cursor()
+            cur.execute('INSERT INTO imagen (nombre, formato, ruta, usuarioidUsuario) VALUES (%s, %s, %s, %s)',
+                    (filename, 'jpg', f"/uploads/{filename}", 1))
+            mysql.connection.commit()
+            
+        return redirect('admin.html')
+    else:
+        return redirect('index.html') 
 
 @app.route('/delete_image')
 def delete_image():
     return 'delete_image'
 
 if __name__ == '__main__':
+    app.secret_key = "llave"
     app.run(port = 3000, debug = True)
